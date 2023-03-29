c5f17d4b6566be0665c921f6ab5a9ca8ced7cb95
laptinho <camsanbar@gmail.com>
2023-03-29 17:03:20 -0300

    ADD: added test pictures for home body

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Nav from '../components/nav/Nav'
+import Navbar from '../components/nav/Navbar'
 
 function Header() {
   return (
-    <header className='flex justify-between items-center px-2 py-1'>
-      <Nav />
+    <header className='flex justify-between items-center px-2 py-1 absolute w-full'>
+      <Navbar />
       <img className="w-13 h-6" src="/src/assets/Logo.png" alt="minga logo" />
     </header>
   )
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="home-header bg-center bg-cover h-screen md:h-[70vh] md:bg-top flex justify-center items-center text-white">
+        <div className='w-[80%] max-w-lg text-center'>
+          <h1 className='font-bold text-3xl'>Your favorite comic book store</h1>
+          <p className='text-sm leading-4 mt-4'>From classics to novelties, we have everything you need to immerse yourself in your favorite universes. Explore our catalog and live the adventure of your life.</p>
+        </div>
+      </div>
+      {/* Last comic uploaded or most popular */}
+      <div className="home-body">
+        {/* TODO: create body from figma styles */}
+      </div>
+    </div>
   )
 }
 
